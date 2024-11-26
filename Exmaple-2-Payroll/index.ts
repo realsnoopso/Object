c68e074459acff008d234763ab76cd042dd0389a
realsnoopso <real@snoop.so>
2024-11-26 13:52:05 +0900

    feat: complete - functional decomposition

--- Exmaple-2-Payroll/index.ts
+++ Exmaple-2-Payroll/index.ts
@@ -1,5 +1,10 @@
 import * as readline from 'readline/promises';
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const main = async (name: string) => {
     const taxRate = await getTaxRate();
     const pay = calculatePayFor(name, taxRate);
@@ -11,23 +16,19 @@ const employees = [
 ]
 
 const getTaxRate = async () => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
     const answer = await rl.question('세율을 입력하세요: ');
     rl.close();
     return Number(answer);
 }
 
 const calculatePayFor = (name:string, taxRate:number)=> {
-    const payroll = employees.find((employee)=> employee.name === name)?.payroll
-    return 0
+    const basePay = employees.find((employee)=> employee.name === name)?.payroll;
+    if (!basePay) return 0;
+    return (basePay - (basePay * taxRate))
 }
 
 const describeResult = (name:string, pay: number)=> {
-
+    rl.write(`이름: ${name}, 급여: ${pay}`)
 }
 
 main('소민경').catch(console.error);
